bdc3a54055bbfcaf84d32a2239dc47b6e7b65e67
Inom <inomoz@users.noreply.github.com>
2021-08-17 20:24:34 +0300

    Create config-initialize.sh

---
+++ roles/dotfiles/tasks/config-initialize.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git init --bare $HOME/.projects
+alias config='/usr/bin/git --git-dir=$HOME/.projects/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.projects/ --work-tree=$HOME'" >> $HOME/.bashrc
