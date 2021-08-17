3ed4c8040ea6e48699ad8a57645ddb905363ec5c
Inom <inomoz@users.noreply.github.com>
2021-08-17 20:23:43 +0300

    Delete config-initialize.sh

--- config-initialize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init --bare $HOME/.projects
-alias config='/usr/bin/git --git-dir=$HOME/.projects/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-echo "alias config='/usr/bin/git --git-dir=$HOME/.projects/ --work-tree=$HOME'" >> $HOME/.bashrc
